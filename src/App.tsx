75c615030047c6e321d09ee6685786522e02c52d
Dan Thackray <djthackray@gmail.com>
2022-08-23 15:34:36 +0100

    Testing Typescript types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import './App.css';
 
+let name: string
+let age: number
+let isStudent: boolean
+let hobbies: number[]
+let role: [number, string]
+
+type Person = {
+  name: string,
+  age?: number
+}
+
+let person: Person = {
+  name: "Dan",
+}
+
+let manyPeople: Person[]
+
+name = "Dan"
+age = 32
+
 function App() {
   return (
     <div className="App">

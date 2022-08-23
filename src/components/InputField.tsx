0b269b90c5bc8fe56ea69c2661fc641b6e2d8d4d
Dan Thackray <djthackray@gmail.com>
2022-08-23 15:57:03 +0100

    Add extra css for input component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import "./styles.css"
 
 const InputField = () => {
   return (
-    <div>InputField</div>
+    <form className="input">
+        <input type="input" placeholder="Enter a task" className="input__box"></input>
+        <button className="input__submit" type="submit">Go</button>
+    </form>
   )
 }
 

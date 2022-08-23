0f075a655ad3b92741eef36d08bc0d3198750e7f
Dan Thackray <djthackray@gmail.com>
2022-08-23 15:49:45 +0100

    Add initial CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,10 @@
 import React from 'react';
 import './App.css';
 
-let name: string
-let age: number | string
-let isStudent: boolean
-let hobbies: number[]
-let role: [number, string]
-
-// type Person = {
-//   name: string,
-//   age?: number
-// }
-
-// let person: Person = {
-//   name: "Dan",
-// }
-
-// let manyPeople: Person[]
-
-// let printName: (name: string) => void
-
-// name = "Dan"
-// age = ""
-
-interface Person {
-  name: string,
-  age?: number
-}
-
-interface Guy extends Person {
-  profession: string
-}
-
-
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      Hello World
+      <span className="heading">Taskify</span>
     </div>
   );
 }

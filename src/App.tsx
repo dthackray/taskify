ac26b9e42ea4f75ff816c3fd85dd87694bc03e3f
Dan Thackray <djthackray@gmail.com>
2022-08-24 13:19:00 +0100

    Add useRef with Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { createArrowFunction } from 'typescript';
 import './App.css';
 import InputField from './components/InputField';
 import { Todo } from './model';
@@ -7,12 +8,22 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
   const [todos, setTodos] = useState<Todo[]>([])
 
-  console.log(todo)
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }])
+      setTodo("")
+    }
+  }
+
+  console.log(todos)
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map(t=><li>{t.todo}</li>)}
     </div>
   );
 }
